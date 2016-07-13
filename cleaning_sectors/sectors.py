d308c42b34a3232ffcc6adee1e11d769474e757a
Marco Cardacci <marcoc@ticketech.local>
2016-07-13 17:31:10 -0400

    unfinished sector class

---
+++ cleaning_sectors/sectors.py
@@ -0,0 +1,11 @@
+
+Kitchen: 
+- Sweep Floor 
+- Swiffer Floor 
+- Wipe down faucet 
+- Wipe down counter top 
+- Wipe down sink 
+- If trash cans are stinky: spray with cleaner, let dry, wipe down with moist towel 
+- Wipe down refrigerator 
+- Wipe down stove top and door 
+-
